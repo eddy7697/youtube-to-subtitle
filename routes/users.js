d61ea82ae8c2394b7bebd62732b3da1a3b510afb
Eddy <vincent7697@gmail.com>
2020-01-30 23:39:15 +0800

    first commit

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var youtubeStream = require('youtube-audio-stream')
+var fs = require('fs');
+ 
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  var requestUrl = 'https://www.youtube.com/watch?v=amYeSaYkSWg'
+  var writeStream = fs.createWriteStream('./output.mp3');
+  try {
+    youtubeStream(requestUrl).pipe(writeStream)
+    res.send('save success')
+  } catch (exception) {
+    res.status(500).send(exception)
+  }
+});
+
+module.exports = router;

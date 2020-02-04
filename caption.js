d3d36b6978ccec6979f93f3a0acb60fac5932cd1
Eddy <vincent7697@gmail.com>
2020-02-05 01:14:50 +0800

    update caption methods

--- caption.js
+++ caption.js
@@ -28,26 +28,30 @@ const request = {
     audio: audio,
 };
 
-// Detects speech in the audio file. This creates a recognition job that you
-// can wait for now, or get its result later.
-const [operation] = await client.longRunningRecognize(request);
+async function startCaption() {
+    // Detects speech in the audio file. This creates a recognition job that you
+    // can wait for now, or get its result later.
+    const [operation] = await client.longRunningRecognize(request);
 
-// Get a Promise representation of the final result of the job
-const [response] = await operation.promise();
-response.results.forEach(result => {
-    console.log(`Transcription: ${result.alternatives[0].transcript}`);
-    result.alternatives[0].words.forEach(wordInfo => {
-        // NOTE: If you have a time offset exceeding 2^32 seconds, use the
-        // wordInfo.{x}Time.seconds.high to calculate seconds.
-        const startSecs =
-            `${wordInfo.startTime.seconds}` +
-            `.` +
-            wordInfo.startTime.nanos / 100000000;
-        const endSecs =
-            `${wordInfo.endTime.seconds}` +
-            `.` +
-            wordInfo.endTime.nanos / 100000000;
-        console.log(`Word: ${wordInfo.word}`);
-        console.log(`\t ${startSecs} secs - ${endSecs} secs`);
+    // Get a Promise representation of the final result of the job
+    const [response] = await operation.promise();
+    response.results.forEach(result => {
+        console.log(`Transcription: ${result.alternatives[0].transcript}`);
+        result.alternatives[0].words.forEach(wordInfo => {
+            // NOTE: If you have a time offset exceeding 2^32 seconds, use the
+            // wordInfo.{x}Time.seconds.high to calculate seconds.
+            const startSecs =
+                `${wordInfo.startTime.seconds}` +
+                `.` +
+                wordInfo.startTime.nanos / 100000000;
+            const endSecs =
+                `${wordInfo.endTime.seconds}` +
+                `.` +
+                wordInfo.endTime.nanos / 100000000;
+            console.log(`Word: ${wordInfo.word}`);
+            console.log(`\t ${startSecs} secs - ${endSecs} secs`);
+        });
     });
-});
+}
+
+startCaption();

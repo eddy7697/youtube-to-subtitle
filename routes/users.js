24c30ac3b0ac61831541773f592b766ca4bff6ae
Eddy <vincent7697@gmail.com>
2020-02-06 00:09:06 +0800

    update

--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,18 @@ var fs = require('fs');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  var requestUrl = 'https://www.youtube.com/watch?v=amYeSaYkSWg'
+  var requestUrl = 'https://www.youtube.com/watch?v=47JiyRa4kog'
   var writeStream = fs.createWriteStream('./output.mp3');
   try {
+    
     youtubeStream(requestUrl).pipe(writeStream)
-    res.send('save success')
+
+    writeStream.on('finish', function () { 
+      res.send('save success')
+    });
+    // res.send('save success')
   } catch (exception) {
-    res.status(500).send(exception)
+    res.status(500).send(exception.message)
   }
 });
 
